7aad32c73764090f03e814c91955145ef6bd8e1a
Byter <avishai.kk@gmail.com>
2020-04-23 17:24:36 +0300

    user signing up and login in

--- controllers/customer.js
+++ controllers/customer.js
@@ -1,8 +1,66 @@
-const Customer = require('../models/customer');
+const Customer = require("../models/customer");
 
 class CustomerController {
-	static signUp(req, res, next) {}
-	static login(req, res, next) {}
+   static async signUp(req, res, next) {
+      const { fullName, email, password } = req.body;
+
+      let existingCustomer;
+      try {
+         existingCustomer = await Customer.findOne({ email });
+      } catch (err) {
+         return res
+            .status(500)
+            .json({ msg: "Signing up failed, please try again later." });
+      }
+
+      if (existingCustomer) {
+         return res
+            .status(422)
+            .json({ msg: "User exists already, please login instead." });
+      }
+
+      const createdCustomer = new Customer({
+         fullName,
+         email,
+         password,
+      });
+
+      try {
+         await createdCustomer.save();
+      } catch (err) {
+         return res
+            .status(500)
+            .json({ msg: "Signing up failed, please try again later." });
+      }
+
+      res.status(201).json({
+         user: createdCustomer.toObject({ getters: true }),
+      });
+   }
+
+   static async login(req, res, next) {
+      const { email, password } = req.body;
+
+      let existingCustomer;
+      try {
+         existingCustomer = await Customer.findOne({ email });
+      } catch (err) {
+         return res.status(500).json({
+            msg: "Logging in failed, please try again later.",
+         });
+      }
+
+      if (!existingCustomer || existingCustomer.password !== password) {
+         return res.status(401).json({
+            msg: "Invalid credentials, could not log you in.",
+         });
+      }
+
+      res.json({
+         msg: "Logged in!",
+         user: existingCustomer.toObject({ getters: true }),
+      });
+   }
 }
 
 module.exports = CustomerController;
