b506cf2ce391ce1f2d9263d8829edb6df9bcaa14
Cringey Boy <avishai.kk@gmail.com>
2020-04-23 12:07:35 +0300

    Changed the morgan's logging method from 'combined' to 'dev'

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(morgan("combined"));
+app.use(morgan("dev"));
 
 app.use("/api/users", customersRoutes);
 app.use("/api/carts", cartsRoutes);
