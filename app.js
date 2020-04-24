8dfaf471354fb9d8dcac6fbd633df929105e1f43
Audrick <audrickong.ao@gmail.com>
2020-04-24 07:04:52 +0700

    add hashing feature and fix errors

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 // get env CONFIG
-if (process.env.NODE_ENV == "development") {
-   require("dotenv").config();
-}
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV || 'prod'}`,
+});
 
 // all module imports
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const morgan = require("morgan");
-const routes = require("./routes");
-
-const { errorHandler } = require("./middlewares/errorHandler");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const routes = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
 
 // other variables
 const app = express();
@@ -21,14 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.use(routes);
-
 app.use(errorHandler);
 
 // connection
 mongoose
-   .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-   .then(() => app.listen(PORT, () => console.log(`http://localhost:${PORT}`)))
-   .catch(console.log);
+	.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => app.listen(PORT, () => console.log(`http://localhost:${PORT}`)))
+	.catch(console.log);
--- controllers/item.js
+++ controllers/item.js
@@ -1,130 +1,130 @@
-const mongoose = require("mongoose");
-const { validationResult } = require("express-validator");
+const mongoose = require('mongoose');
+const { validationResult } = require('express-validator');
 
-const Item = require("../models/item");
-const Cart = require("../models/cart");
-const Product = require("../models/product");
+const Item = require('../models/item');
+const Cart = require('../models/cart');
+const Product = require('../models/product');
 
 class ItemController {
-   static async create(req, res, next) {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-         next({
-            msg: "Invalid inputs passed, please check your data",
-            code: 422,
-         });
-      }
+	static async create(req, res, next) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			next({
+				msg: 'Invalid inputs passed, please check your data',
+				code: 422,
+			});
+		}
 
-      const { cid: cartId, pid: productId } = req.params;
-      const quantity = req.body.quantity;
+		const { cid: cartId, pid: productId } = req.params;
+		const quantity = req.body.quantity;
 
-      let product;
-      try {
-         product = await Product.findById(productId);
-      } catch (err) {
-         next({ msg: "Creating item failed, please try again.", code: 500 });
-      }
+		let product;
+		try {
+			product = await Product.findById(productId);
+		} catch (err) {
+			next({ msg: 'Creating item failed, please try again.', code: 500 });
+		}
 
-      if (!product) {
-         next({
-            msg: "Could not find a product for the provided id",
-            code: 404,
-         });
-      }
+		if (!product) {
+			next({
+				msg: 'Could not find a product for the provided id',
+				code: 404,
+			});
+		}
 
-      let cart;
-      try {
-         cart = await Cart.findById(cartId).populate("items");
-      } catch (err) {
-         next({ msg: "Creating item failed, please try again.", code: 500 });
-      }
+		let cart;
+		try {
+			cart = await Cart.findById(cartId).populate('items');
+		} catch (err) {
+			next({ msg: 'Creating item failed, please try again.', code: 500 });
+		}
 
-      if (!cart) {
-         next({ msg: "Could not find a cart for the provided id", code: 404 });
-      }
+		if (!cart) {
+			next({ msg: 'Could not find a cart for the provided id', code: 404 });
+		}
 
-      const createdItem = new Item({
-         productId = product.id,
-         cartId = cart.id,
-         quantity
-      });
+		const createdItem = new Item({
+			productId: product.id,
+			cartId: cart.id,
+			quantity,
+		});
 
-      try {
-         const session = await mongoose.startSession();
-         session.startTransaction();
-         await createdItem.save({session});
-         await cart.items.push(createdItem);
-         await cart.save({session});
-         session.commitTransaction();
-      } catch(err) {
-         next({msg: "Creating item failed, please try again", code: 500})
-      }
+		try {
+			const session = await mongoose.startSession();
+			session.startTransaction();
+			await createdItem.save({ session });
+			await cart.items.push(createdItem);
+			await cart.save({ session });
+			session.commitTransaction();
+		} catch (err) {
+			next({ msg: 'Creating item failed, please try again', code: 500 });
+		}
 
-      res.status(201).json({item: createdItem.toObject({getters: true})});
-   }
+		res.status(201).json({ item: createdItem.toObject({ getters: true }) });
+	}
 
-   static async update(req, res, next) {
-      const errors = validationResult(req);
-      if(!errors.isEmpty()) {
-         next({
-            msg: "Invalid inputs passed, please check your data",
-            code: 422,
-         });
-      }
+	static async update(req, res, next) {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			next({
+				msg: 'Invalid inputs passed, please check your data',
+				code: 422,
+			});
+		}
 
-      const itemId = req.params.iid;
-      const quantity = req.body.quantity;
+		const itemId = req.params.iid;
+		const quantity = req.body.quantity;
 
-      let item;
-      try {
-         item = await Item.findById(itemId);
-      } catch(err) {
-         next({msg: "Updating item failed, please try again.", code: 500});
-      }
+		let item;
+		try {
+			item = await Item.findById(itemId);
+		} catch (err) {
+			next({ msg: 'Updating item failed, please try again.', code: 500 });
+		}
 
-      if(!item) {
-         next({msg: "Could not find an item for the provided id.", code: 404});
-      }
+		if (!item) {
+			next({ msg: 'Could not find an item for the provided id.', code: 404 });
+		}
 
-      item.quantity = quantity;
+		item.quantity = quantity;
 
-      try {
-         const session = mongoose.startSession();
-         await item.save({session});
-         await item.cartId.save({session});
-      } catch(err) {
-         next({msg: "Updating item failed, please try again.", code: 500});
-      }
+		try {
+			const session = mongoose.startSession();
+			await item.save({ session });
+			await item.cartId.save({ session });
+		} catch (err) {
+			next({ msg: 'Updating item failed, please try again.', code: 500 });
+		}
 
-      res.json({item: item.toObject({getters: true})});
-   }
+		res.json({ item: item.toObject({ getters: true }) });
+	}
 
-   static async delete(req, res, next) {
-      const itemId = req.params.iid;
+	static async delete(req, res, next) {
+		const itemId = req.params.iid;
 
-      let item;
-      try {
-         item = await Item.findById(itemId).populate("cartId");
-      } catch(err) {
-         next({msg: "Deleting item failed, please try again.", code: 500});
-      }
+		let item;
+		try {
+			item = await Item.findById(itemId).populate('cartId');
+		} catch (err) {
+			next({ msg: 'Deleting item failed, please try again.', code: 500 });
+		}
 
-      if(!item) {
-         next({msg: "Could not find an item for the provided id.", code: 404});
-      }
+		if (!item) {
+			next({ msg: 'Could not find an item for the provided id.', code: 404 });
+		}
 
-      try {
-         const session = mongoose.startSession();
-         await item.cartId.items.pull(item);
-         await item.cartId.save({session});
-         await item.remove({session});
-         await session.commitTransaction();
-      } catch(err) {
-         next({msg: "Deleting item failed, please try again.", code: 500});
-      }
+		try {
+			const session = mongoose.startSession();
+			await item.cartId.items.pull(item);
+			await item.cartId.save({ session });
+			await item.remove({ session });
+			await session.commitTransaction();
+		} catch (err) {
+			next({ msg: 'Deleting item failed, please try again.', code: 500 });
+		}
 
-      res.json({msg: "Deleted item."});
-   }
+		res.json({ msg: 'Deleted item.' });
+	}
 }
 
 module.exports = ItemController;
