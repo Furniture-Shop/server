de95a9cf5bca6fd71b14264f26c03919d4df34ec
Byter <avishai.kk@gmail.com>
2020-04-27 13:08:24 +0300

    Bug fixing + Cannot have more then one same product in a cart => Instead will increase the quantity

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // get env CONFIG
-require('dotenv').config({
-	path: `.env.${process.env.NODE_ENV || 'prod'}`,
+require("dotenv").config({
+   path: `.env.${process.env.NODE_ENV || "prod"}`,
 });
 
 // all module imports
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const morgan = require('morgan');
-const routes = require('./routes');
-const errorHandler = require('./middlewares/errorHandler');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const routes = require("./routes");
+const errorHandler = require("./middlewares/errorHandler");
 
 // other variables
 const app = express();
@@ -20,19 +20,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(routes);
 app.use(errorHandler);
 
 // Deprecation warning fix
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 
 // connection
 mongoose
-	.connect(MONGODB_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => app.listen(PORT, () => console.log(`http://localhost:${PORT}`)))
-	.catch(console.log);
+   .connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   })
+   .then(() => app.listen(PORT, () => console.log(`http://localhost:${PORT}`)))
+   .catch(console.log);
