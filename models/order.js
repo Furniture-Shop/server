383d5a2d32a773d8968c84da76926da0ce87d5e9
domssilva <silva@programmer.net>
2020-04-30 13:29:22 +0200

    create order schema

---
+++ models/order.js
@@ -0,0 +1,12 @@
+const { Schema, model, Types } = require('mongoose');
+
+const orderSchema = new Schema({
+  totalPrice: { type: Number, required: true },
+  customer: { type: String, required: true },
+  invoiceDate: { type: Date, default: Date.now },
+  invoiceNo: { type: Number, required: true, min: 0 },
+  items: [{ type: Types.ObjectId, required: true, ref: 'Product' }],
+  status: { type: String, require: true }
+});
+
+module.exports = model('Order', orderSchema);
